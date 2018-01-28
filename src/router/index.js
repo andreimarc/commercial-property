33212868546d964f7ed9c289f9163be890101808
Andrei Marchevca <andrei.marchevca@tobii.com>
2018-01-28 22:43:27 +0100

    router file update with correct component

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import MainCard from '@/components/MainCard'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'MainCard',
+      component: MainCard
     }
   ]
 })
