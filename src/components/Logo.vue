b3403a859d5177e21fe76fa3953a1864e4afbaf6
Andrei Marchevca <andrei.marchevca@tobii.com>
2018-01-28 23:08:13 +0100

    Logo component

---
+++ src/components/Logo.vue
@@ -0,0 +1,19 @@
+<template>
+  <img class="bc-Logo" src="../assets/images/logo.png">
+</template>
+
+<script>
+// App logo component
+export default {
+  name: 'Logo'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.bc-Logo {
+  float: left;
+  width: 209px;
+  margin: 5px 10px;
+}
+</style>
