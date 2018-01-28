bdecc4b6734e105a3814b7e445cb15c61804b05a
Andrei Marchevca <andrei.marchevca@tobii.com>
2018-01-28 23:09:14 +0100

    Subheaders

---
+++ src/components/Subheader.vue
@@ -0,0 +1,24 @@
+<template>
+  <h2 class="bc-Subheader">{{ message }}</h2>
+</template>
+
+<script>
+// h2 headers
+export default {
+  name: 'Subheader',
+  props: {
+    message: {
+      default: 'This is a subheader.',
+      type: String
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.bc-Subheader {
+  display: block;
+  text-align: left;
+}
+</style>
